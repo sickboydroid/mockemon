016aba8a26d34345a993bb162678b2d06b2c13c1
sickboy <sickboydroid@gmail.com>
2025-03-26 19:48:39 +0530

    make video element trapped in main content of page

--- src/scripts/components/draggable-widgets/liveControls.js
+++ src/scripts/components/draggable-widgets/liveControls.js
@@ -92,7 +92,7 @@ export function setupLiveControls() {
       inertia: true,
       modifiers: [
         interact.modifiers.restrictRect({
-          restriction: "parent",
+          restriction: document.querySelector(".content"),
         }),
       ],
     });
